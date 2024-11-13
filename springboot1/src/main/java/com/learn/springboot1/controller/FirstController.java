75fe705ec16b46727205c0ad6b10fcab07d7b1d6
netralichorge <netrali.chorge20@gmail.com>
2024-11-13 08:24:39 +0530

    @PathVariable, @Postmapping

--- springboot1/src/main/java/com/learn/springboot1/controller/FirstController.java
+++ springboot1/src/main/java/com/learn/springboot1/controller/FirstController.java
@@ -1,11 +1,17 @@
 package com.learn.springboot1.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learn.springboot1.entity.Student;
 
-// we have two annottions - 1) @Controller  2) @RestController
+// we have two annotations - 1) @Controller  2) @RestController
 
 @RestController
 public class FirstController {
@@ -31,5 +37,33 @@ public class FirstController {
 		s.setName("Nisha");
 		return s;
 	}
-
+	
+	@GetMapping("/all-student")
+	public List<Student> allStudent(){
+		
+		ArrayList<Student> student = new ArrayList<Student>();
+		student.add(new Student(101, "Nisha", 60));
+		student.add(new Student(102, "Janki", 80));
+		return student;
+	}
+	
+	
+	// for multiple id
+	 //        ("/students/100")
+	
+	@GetMapping("/students/{id}") // path variable
+	public String learnPathVariable(@PathVariable int id)  // java variable
+	{
+		System.out.println(id);         // id print on console
+		return "Data fetched successfully"; // return statement print on postman
+	}
+	
+	// Post used to add data
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) // @RequestBody used for read data
+	{
+		System.out.println(student);
+		return student;
+	}
 }
+
--- springboot1/src/main/java/com/learn/springboot1/entity/Student.java
+++ springboot1/src/main/java/com/learn/springboot1/entity/Student.java
@@ -1,8 +1,12 @@
 package com.learn.springboot1.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data  // used for create getter setters with the help of lombok.
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 	
 	private int id;
