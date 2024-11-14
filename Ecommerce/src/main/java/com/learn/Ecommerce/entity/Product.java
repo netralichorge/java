de21a101798bc15c4d82cd8df750f52bded89258
netralichorge <netrali.chorge20@gmail.com>
2024-11-14 08:59:01 +0530

    Product table

---
+++ Ecommerce/src/main/java/com/learn/Ecommerce/entity/Product.java
@@ -0,0 +1,33 @@
+package com.learn.Ecommerce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data     // @Data annotation create POJO class
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity   // Entity annotation to create table
+//@Table(name = "product_details") 
+// if you want change table name use Table annotation
+// table name will be same as class name.
+
+public class Product {
+	
+	@Id  // apply for primary key on table
+	private int productId;
+	
+	@Column(nullable = false)
+	private String productName;
+	
+	@Column(name = "description", nullable = false)
+	private String productDescription;
+	
+	@Column(nullable = false)
+	private int productPrice;
+
+}
