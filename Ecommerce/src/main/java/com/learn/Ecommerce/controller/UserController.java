c6c7e1e36ddf1a38f8affb70a3d49e1c705af132
netralichorge <netrali.chorge20@gmail.com>
2024-11-19 08:55:03 +0530

    stored values on database

--- Ecommerce/src/main/java/com/learn/Ecommerce/controller/UserController.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/controller/UserController.java
@@ -1,12 +1,30 @@
 package com.learn.Ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.Ecommerce.Dto.UserDto;
+import com.learn.Ecommerce.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	@Autowired
+	UserService userService;
 	
+	//POST["/users"]
+	
+	@PostMapping
+	public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto)
+	{
+		UserDto savedUser = userService.addUser(userDto);
+		return new ResponseEntity<UserDto>(savedUser,HttpStatus.CREATED);
+	}
 	
 
 }
--- Ecommerce/src/main/java/com/learn/Ecommerce/service/UserServiceImpl.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.learn.Ecommerce.Dto.UserDto;
 import com.learn.Ecommerce.entity.User;
 import com.learn.Ecommerce.repository.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
