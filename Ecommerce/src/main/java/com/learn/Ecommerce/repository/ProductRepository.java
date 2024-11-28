4f11e7590c0abec03c213317122868c745536df6
netralichorge <netrali.chorge20@gmail.com>
2024-11-28 08:57:00 +0530

    Creating url

--- Ecommerce/src/main/java/com/learn/Ecommerce/repository/ProductRepository.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/repository/ProductRepository.java
@@ -1,9 +1,27 @@
 package com.learn.Ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
+	List<Product> findByProductPriceLessThan(int price);
+	
+	List<Product> findByProductPriceGreaterThan(int price);
+	
+	List<Product> findByProductPriceLessThanEqual(int price);
+	
+	List<Product> findByProductPriceGreaterThanEqual(int price);
+	
+	List<Product> findByProductPriceBetween(int startPrice,int endPrice);
+	
 
 }
+
+// GET POST / products
+//GET /products/{id}
+//PUT
+//DELETE
--- Ecommerce/src/main/java/com/learn/Ecommerce/repository/UserRepository.java
+++ Ecommerce/src/main/java/com/learn/Ecommerce/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package com.learn.Ecommerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn.Ecommerce.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String>{
 	
+	Optional<User> findByEmailId(String email);
+	
+	List<User> findByFirstname(String firstname);
+	
+	List<User> findByLastName(String firstName);
 	
 
 }
